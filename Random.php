c74ec6e45065641bec34171aa752760244f08150
Sukruth M K <suk_mallur@yahoo.co.in>
2018-12-10 11:17:42 -0500

    Added random integer generator function

--- Random.php
+++ Random.php
@@ -7,5 +7,42 @@ Class Random {
     function __construct() {
 
     }
+
+    /**
+     * Function to integer number
+     * @param float $Min Minimal value
+     * @param float $Max Maximal value
+     * @return float Random int value
+     */
+    function int($min = false, $max = false) {
+        // return random number within range
+        if($min && $max) {
+            return mt_rand($min, $max);
+        }
+
+        // return random number without range
+        return mt_rand();
+    }
+
+    /**
+     * Function to generate floating number
+     * @param float $Min Minimal value
+     * @param float $Max Maximal value
+     * @param int $precision The optional number of decimal digits to round to. default false means not round
+     * @return float Random float value
+     */
+    function float($min = false, $max = false, $precision = false) {
+        if($min && $max) {
+            $randomfloat = $min + mt_rand() / mt_getrandmax() * ($max - $min);
+        } else {
+            $randomfloat = mt_rand() / mt_getrandmax();
+        }
+
+    	if($precision) {
+    		$randomfloat = round($randomfloat,$round);
+        }
+
+    	return $randomfloat;
+    }
 }
 ?>
